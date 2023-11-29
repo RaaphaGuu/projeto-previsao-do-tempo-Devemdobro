39e75da777d68e56a62c50e0de0892d2fbe44eb7
Raphael Feliciano <raphaelgustavo@live.com>
2023-11-28 23:27:10 -0300

    primeiro-commit

---
+++ src/js/index.js
@@ -0,0 +1,51 @@
+const chaveDaApi = "728bdd18b3bc411eb5d94711232711";
+
+const botaoDeBusca = document.querySelector(".btn-buscar");
+
+botaoDeBusca.addEventListener("click", async () => {
+
+const cidade = document.getElementById("input-busca").value;
+  if (!cidade) return;
+	
+const dados = await buscarDadosDaCidade(cidade);
+
+	document.getElementById("input-busca").value = "";
+
+  if (dados) preencherDadosNaTela(dados, cidade) ;
+
+
+});
+
+	async function buscarDadosDaCidade(cidade) {
+	const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${chaveDaApi}&q=${cidade}&aqi=no&lang=pt`;
+	
+	
+
+  const resposta = await fetch(apiUrl);
+
+  if (resposta.status !== 200) return;
+
+  const dados = resposta.json();
+
+  return dados;
+		
+}
+	function preencherDadosNaTela(dados, cidade) {
+		const temperatura = dados.current.temp_c;
+		const condicao = dados.current.condition.text;
+		const humidade = dados.current.humidity;
+		const velocidadeDoVento = dados.current.wind_kph;
+		const iconeCondicao = dados.current.condition.icon;
+
+  document.getElementById("cidade").textContent = cidade;
+ 	document.getElementById("temperatura").textContent = `${temperatura} ºC`;
+	document.getElementById("condicao").textContent = condicao;
+	document.getElementById("humidade").textContent = `${humidade}%`;
+	document.getElementById(
+    "velocidade-do-vento"
+  ).textContent = `${velocidadeDoVento} km/h`;
+	document.getElementById("icone-condicao").setAttribute("src", iconeCondicao);
+	
+}
+
+
